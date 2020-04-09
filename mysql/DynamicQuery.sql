0cdbf93624502a58ebfd0a9382700f9c5008aa84
Ehsan Mohammadi <ehsanx64@gmail.com>
2020-04-09 16:22:12 +0430

    Added more details/customizations

--- mysql/DynamicQuery.sql
+++ mysql/DynamicQuery.sql
@@ -24,16 +24,22 @@
 */
 
 -- Specify which fields to select
-set @fields = ' ID, post_title, post_type ';
+set @fields = ' ID as id, post_type as type, post_title as title ';
 
 -- Specify target table
 set @table = ' from wp_posts';
 
 -- Exclude a list of post types ('attachment', 'revision', 'nav_menu_item')
-set @condition = ' where post_type not in (\'attachment\', \'revision\', \'nav_menu_item\') ';
+set @condition = ' where post_type not in (
+    \'attachment\', \'revision\', \'nav_menu_item\', \'oembed_cache\',
+    \'option-tree\', \'polylang_mo\') ';
+
+-- Following method is also valid for creating a user-defined variable but it
+-- echoes the expression value to console.
+select @order := ' order by id asc';
 
 -- Create a query (string) from variables
-set @q = concat('select', @fields, @table, @condition);
+set @q = concat('select', @fields, @table, @condition, @order);
 
 -- Prepare a statement from query string and execute it
 prepare stmt from @q;
